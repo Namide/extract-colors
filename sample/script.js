c08d90f791f4bc77a405f81bf33dfcb79f9a7a60
Namide <damien.git@gmail.com>
2020-02-25 23:12:00 +0100

    add examples

--- sample/script.js
+++ sample/script.js
@@ -1,56 +1,65 @@
 import extractColors from '../src/extractColorsBrowser'
 
+// Add image + colors in DOM
 const display = (colors, src) => {
+  const div = document.createElement('div')
+  div.classList.add('block')
+  document.body.appendChild(div)
+
+  // display image
   const image = new Image()
   image.src = src
   image.height = 200
   image.style.width = 'auto'
-  document.body.appendChild(image)
+  div.appendChild(image)
 
-  const canvas = document.createElement('canvas')
-  canvas.width = 20
-  canvas.height = image.height
+  // display colors
+  {
+    const canvas = document.createElement('canvas')
+    canvas.width = 20
+    canvas.height = image.height
 
-  const context = canvas.getContext('2d')
+    const context = canvas.getContext('2d')
 
-  var imageData = context.createImageData(canvas.width, canvas.height)
-  var pixels = imageData.data
+    const imageData = context.createImageData(canvas.width, canvas.height)
+    const pixels = imageData.data
 
-  /* eslint no-labels: "off" */
-  loop: for (var r = 0; r < imageData.height; r++) {
-    for (var c = 0; c < imageData.width; c++) {
-      const i = Math.floor(r / canvas.width)
-      const color = colors[i]
+    /* eslint no-labels: "off" */
+    loop: for (let r = 0; r < imageData.height; r++) {
+      for (let c = 0; c < imageData.width; c++) {
+        const i = Math.floor(r / canvas.width)
+        const color = colors[i]
 
-      if (!color) { break loop }
+        if (!color) { break loop }
 
-      // Calculate the position of the current pixel in the array
-      var pos = (r * (imageData.width * 4)) + (c * 4)
-
-      // Assign the colour to each pixel
-      pixels[pos + 0] = color.red
-      pixels[pos + 1] = color.green
-      pixels[pos + 2] = color.blue
-      pixels[pos + 3] = 255
+        const pos = (r * (imageData.width * 4)) + (c * 4)
+        pixels[pos + 0] = color.red
+        pixels[pos + 1] = color.green
+        pixels[pos + 2] = color.blue
+        pixels[pos + 3] = 255
+      }
     }
+
+    context.putImageData(imageData, 0, 0)
+    div.appendChild(canvas)
   }
-
-  context.putImageData(imageData, 0, 0)
-
-  document.body.appendChild(canvas)
 }
 
-['test', 'jean', 'crayon', 'jumelle', 'journey', 'park'].forEach(seed => {
-  const imgSrc = `https://picsum.photos/seed/${seed}/200/300`
-  extractColors(imgSrc, { crossOrigin: 'anonymous' })
-    .then(colors => {
-      display(colors, imgSrc)
-      console.log(imgSrc)
-      console.log(colors)
-    })
-    .catch(console.log)
+// Some random tests
+['test', 'jean', 'crayon', 'jumelle', 'journey', 'park', 'sun', 'moon', 'water', 'sea', 'colors', 'sky', 'unicorn'].forEach(seed => {
+  [6, 10, 15].forEach(index => {
+    const imgSrc = `https://loremflickr.com/320/240/${seed}/?lock=${index + 5}`
+    extractColors(imgSrc, { crossOrigin: 'anonymous' })
+      .then(colors => {
+        display(colors, imgSrc)
+        console.log(imgSrc)
+        console.log(colors)
+      })
+      .catch(console.log)
+  })
 })
 
+// Your tests in the img directory
 Object.values(require('./img/*.jpg')).forEach(imgSrc => {
   extractColors(imgSrc)
     .then(colors => {
