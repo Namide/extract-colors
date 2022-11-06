fc9a0da3374c43a279cced008ad495bc62ef3a39
Namide <damien.git@gmail.com>
2022-11-07 00:00:18 +0100

    Clean files

--- vite.config.node.ts
+++ vite.config.node.ts
@@ -10,15 +10,7 @@ module.exports = defineConfig({
       fileName: (format) => `extract-colors.node.${format}.js`,
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      // external: ['vue'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        // globals: {
-        //   vue: 'Vue',
-        // },
         dir: './lib'
       },
     },
