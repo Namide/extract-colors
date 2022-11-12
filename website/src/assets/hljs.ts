93101f338a8b9c0191ceb42097d2b5826b330df1
Namide <damien.git@gmail.com>
2022-11-13 00:03:59 +0100

    Init guide

---
+++ website/src/assets/hljs.ts
@@ -0,0 +1,3 @@
+import hljs from "highlight.js/lib/common"
+
+hljs.highlightAll()
--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
+  safelist: [{
+    pattern: /hljs+/,
+  }],
   theme: {
-    extend: {},
+    hljs: {
+      theme: 'monokai',
+    },
   },
   daisyui: {
     themes: ["acid"],
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require('tailwind-highlightjs')],
 }
