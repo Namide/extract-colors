0a511a81cca18310281f967225223a537ad0a58c
Namide <damien.git@gmail.com>
2023-04-10 13:23:17 +0200

    Move directory

--- examples/browser-require/src/index.js
+++ examples/browser-require/src/index.js
@@ -1,7 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
