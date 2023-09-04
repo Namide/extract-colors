3c566a6382f9bdb89fc6019276a0b3986509c26c
Namide <damien.git@gmail.com>
2023-09-04 20:29:01 +0200

    Add comments to main functions

--- src/extractColors.ts
+++ src/extractColors.ts
@@ -10,11 +10,20 @@ type ImageDataAlt = { data: Uint8ClampedArray | number[], width?: number, height
 
 /**
  * Browser or Node.js context detection
+ * 
+ * @returns Is broswer context
  */
 const _isBrowser = () => typeof window !== 'undefined'
 
 /**
  * Sort colors and generate standard list of colors.
+ * 
+ * @param _colors List of colors
+ * @param _pixels Count of pixels in the image
+ * @param _hueDistance Maximal HUE distance between pixel before pixel merging
+ * @param _saturationDistance Maximal saturation distance between pixel before pixel merging
+ * @param _lightnessDistance Maximal lightness distance between pixel before pixel merging
+ * @returns Sorted colors list
  */
 const _sortFinalColors = (_colors: Color[], _pixels: number, _hueDistance: number, _saturationDistance: number, _lightnessDistance: number) => {
   const list = sortColors(_colors, _pixels, _hueDistance, _saturationDistance, _lightnessDistance)
@@ -25,6 +34,10 @@ const _sortFinalColors = (_colors: Color[], _pixels: number, _hueDistance: numbe
  * Extract ImageData from image.
  * Reduce image to a pixel count.
  * Browser only
+ * 
+ * @param _image HTML image element
+ * @param _pixels Count of maximum pixels accepted for the calculation
+ * @returns Data of the reduced image
  */
 const _getImageData = (_image: HTMLImageElement, _pixels: number) => {
   const currentPixels = _image.width * _image.height
@@ -43,6 +56,18 @@ const _getImageData = (_image: HTMLImageElement, _pixels: number) => {
 
 /**
  * Extract colors from an ImageData object.
+ * 
+ * @param imageData Data of the image
+ * @param options Process configuration
+ * @param options.pixels The name of the user.
+ * @param options.distance The email of the user.
+ * @param options.colorValidator Test function to enable only some colors
+ * @param options.saturationDistance Minimum saturation value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.lightnessDistance inimum lightness value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.hueDistance inimum hue value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.crossOrigin support for CORS (only for browser)
+ * 
+ * @returns List of extracted colors
  */
 export const extractColorsFromImageData = (imageData: ImageData | ImageDataAlt, options: NodeOptions | BrowserOptions = {}) => {
   const [_pixels, _distance, _colorValidator, _hueDistance, _saturationDistance, _lightnessDistance] = cleanInputs(options)
@@ -53,6 +78,18 @@ export const extractColorsFromImageData = (imageData: ImageData | ImageDataAlt, 
 /**
  * Extract colors from an HTMLImageElement.
  * Browser only
+ * 
+ * @param image HTML image element
+ * @param options Process configuration
+ * @param options.pixels The name of the user.
+ * @param options.distance The email of the user.
+ * @param options.colorValidator Test function to enable only some colors
+ * @param options.saturationDistance Minimum saturation value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.lightnessDistance inimum lightness value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.hueDistance inimum hue value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.crossOrigin support for CORS (only for browser)
+ * 
+ * @returns List of extracted colors
  */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore 
@@ -91,6 +128,18 @@ export const extractColorsFromImage = (image: HTMLImageElement, options: Browser
 /**
  * Extract colors from a path.
  * The image will be downloaded.
+ * 
+ * @param src Image source
+ * @param options Process configuration
+ * @param options.pixels The name of the user.
+ * @param options.distance The email of the user.
+ * @param options.colorValidator Test function to enable only some colors
+ * @param options.saturationDistance Minimum saturation value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.lightnessDistance inimum lightness value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.hueDistance inimum hue value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.crossOrigin support for CORS (only for browser)
+ * 
+ * @returns List of extracted colors
  */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore 
@@ -113,7 +162,18 @@ export const extractColorsFromSrc = (src: string, options: BrowserOptions = {}):
 
 /**
  * Extract colors from a picture.
- * Node and Browser support
+ * 
+ * @param picture Image, image source or image data (node.js context only support image data)
+ * @param options Process configuration
+ * @param options.pixels The name of the user.
+ * @param options.distance The email of the user.
+ * @param options.colorValidator Test function to enable only some colors
+ * @param options.saturationDistance Minimum saturation value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.lightnessDistance inimum lightness value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.hueDistance inimum hue value between two colors otherwise the colors will be merged (from 0 to 1)
+ * @param options.crossOrigin support for CORS (only for browser)
+ * 
+ * @returns List of extracted colors
  */
 export const extractColors = (picture: string | HTMLImageElement | ImageData | ImageDataAlt, options?: BrowserOptions) => {
 
