3b6b6cc8a2545803164a2be3b0baddaaab3d5e20
Namide <damien.git@gmail.com>
2023-09-04 23:04:54 +0200

    Remove website and examples

--- examples/browser-cjs/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { extractColors } from "extract-colors"
-
-const src = `https://loremflickr.com/640/480?lock=${ Math.floor(Math.random() * 0xFFFFFF) }`
-extractColors(src, { crossOrigin: 'anonymous' }).then((list) => {
-  console.log(list)
-  document.body.innerHTML = JSON.stringify(list, null, '&nbsp;').replace(/\n/ig, '<br>')
-})
